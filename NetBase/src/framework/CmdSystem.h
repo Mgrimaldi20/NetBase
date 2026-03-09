12fbbd4e936d6aa51a10cbd079fbe01d9aaff143
Marco Grimaldi <56061871+Mgrimaldi20@users.noreply.github.com>
2026-03-09 21:35:16 +1100

    remove some stuff thats no longer required, or maybe never was

--- NetBase/src/framework/CmdSystem.h
+++ NetBase/src/framework/CmdSystem.h
@@ -12,14 +12,12 @@
 #include "Log.h"
 #include "ByteBuffer.h"
 
-#include "sys/win32/io/IOContext.h"
-
 /*
 * Class: CmdSystem
 * Handles the parsing and the creation of command objects from incoming data packets.
 * Each command object encapsulates the logic for executing specific commands and acts upon a context.
 * 
-*	RegisterHandler: Registers a handler function for a specific command, allows multiple handlers for the same command type (for acks, etc.)
+*	RegisterHandler: Registers a handler function for a specific command, allows multiple handlers for the same command type
 *	ParseCommand: Parses incoming data to identify the command type and creates the corresponding command object
 */
 class CmdSystem
