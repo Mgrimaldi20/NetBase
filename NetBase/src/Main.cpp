b11112afe4c623f2ba1363ba291668a100649993
Marco Grimaldi <56061871+Mgrimaldi20@users.noreply.github.com>
2026-03-09 22:48:14 +1100

    add some stuff back, also fix an include error

--- NetBase/src/Main.cpp
+++ NetBase/src/Main.cpp
@@ -1,7 +1,62 @@
 #include <iostream>
+#include <string_view>
 
-int main()
+#include "framework/Log.h"
+
+static std::string_view serverport = "5001";	// default port number of the server
+
+static bool ValidateOptions(int argc, char **argv);
+
+int main(int argc, char **argv)
 {
-	std::cout << "Hello World!" << std::endl;
+	if (!ValidateOptions(argc, argv))
+		return 1;
+
+	Log log;
+
+	log.Info("Server port: {}", serverport);
+
 	return 0;
+}
+
+bool ValidateOptions(int argc, char **argv)
+{
+	for (int i=1; i<argc; i++)
+	{
+		if (argv[i][0] != '-')
+			continue;
+
+		switch (argv[i][1])
+		{
+			case 'p':
+			{
+				std::string_view arg(argv[i]);
+
+				if (arg.length() <= 3 || arg[2] != ':')
+				{
+					std::cout << "Invalid port number format: " << argv[i] << std::endl;
+					return false;
+				}
+
+				serverport = arg.substr(3);
+
+				break;
+			}
+
+			case '?':
+				std::cout << std::endl << "Usage:" << std::endl
+					<< "NetBase [-p:<port>] [-?]" << std::endl
+					<< "--------------------------------------------------" << std::endl
+					<< "-p:<port>    Specify the port number of the server" << std::endl
+					<< "-?           Prints out this help message and exit" << std::endl;
+
+				return false;
+
+			default:
+				std::cout << "Unknown command line options flag: " << argv[i] << std::endl;
+				return false;
+		}
+	}
+
+	return true;
 }
